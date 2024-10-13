2e69d0a4cbdb873a737e8072bfed418e3af3bbe3
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:13:10 +0200

    [FIX] Fixed new type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -19,7 +19,7 @@ class TypeSeeder extends Seeder
         foreach($types as $type){
             $new_type = new Type();
             $new_type->name = $type;
-            $new_type->slug = Type::generateSlug($title);
+            $new_type->slug = Type::generateSlug($type);
             $new_type->save();
         }
     }
