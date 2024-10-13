cb5e122ae6aeffddc7b90ec8a0475f1dc8c6e041
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:34:35 +0200

    [UPD] Added types to project controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -75,7 +75,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        return view('admin.projects.edit', compact('project'));
+        return view('admin.projects.edit', compact('project', 'types'));
     }
 
     /**
