404f10f14f0160d8e3df19418212426c4bc579b8
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:33:44 +0200

    [FIX] Fixed scheme table name

--- database/migrations/2024_10_13_195708_add_type_id_column_to_table_projects.php
+++ database/migrations/2024_10_13_195708_add_type_id_column_to_table_projects.php
@@ -26,7 +26,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('table_projects', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->dropForeign('projects_type_id_foreign');
             $table->dropColumn('type_id');
 
