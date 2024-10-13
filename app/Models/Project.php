39e2f797f55c9ed69db9ee83db78c83946e1138f
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:03:23 +0200

    [UPD] Added public function type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,4 +12,8 @@ class Project extends Model
     public static function generateSlug($title){
         return Str::slug($title, '-');
     }
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
