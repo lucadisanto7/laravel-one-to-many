adba293793e37de30c13cd7f672e6cb111b04095
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:37:26 +0200

    [UPD] Added type to show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-12">
             <h2>{{$project->name}}</h2>
+            <h4>{{$project->type !== null ? $project->type->name : ''}}</h4>
             <img class="project-image" src="{{ $project->image !== null ? asset('./storage/'.$project->image) : 'https://plachehold.co/600x400?text=Immagine+copertina'}}" alt="{{ $project->name}}">
             <p>{{$project->slug}}</p>
             <p>{{$project->summary}}</p>
